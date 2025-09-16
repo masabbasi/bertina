d296545c833691ca8e1eef5240b2150e29af309c
Masoud Abbasi <masoud.abbasi.71@gmail.com>
2025-09-16 19:29:41 +0330

    Fix Bug

---
+++ src/constance/mainCategory.js
@@ -0,0 +1,179 @@
+const footerCategories = [
+  {
+    title: "هاست",
+    children: [
+      {
+        title: "هاست",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "خرید سرویس هاست",
+      },
+      {
+        title: "هاست لینوکس",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "جهت استفاده از وردپرس و زبان PHP",
+      },
+      {
+        title: "هاست ویندوز",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "مناسب برای زبان‌های ASP و .NET",
+      },
+      {
+        title: "هاست ایمیل",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "ایمیل سازمانی",
+      },
+      {
+        title: "هاست وردپرس",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "سرویس هاست با بهینه‌سازی اختصاصی وردپرس",
+      },
+      {
+        title: "هاست ووکامرس جدید",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "مخصوص ووکامرس و دیگر پلتفرم‌های فروشگاهی",
+      },
+      {
+        title: "نمایندگی هاست",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "ارائه سرویس‌های هاست به مشتریان خود",
+      },
+    ],
+  },
+  {
+    title: "دامنه",
+    children: [],
+  },
+  {
+    title: "تبلیغات در گوگل",
+    children: [
+      {
+        title: "تبلیغات در گوگل",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "لینک اول صفحه‌ی نتایج جستجوی گوگل",
+      },
+      {
+        title: "تبلیغات بنری گوگل",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "نمایش در اپلیکیشن‌ها و سایت‌های پربازدید",
+      },
+      {
+        title: "تبلیغات نصب اپلیکیشن",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "نمایش تبلیغ در اپلیکیشن‌ها",
+      },
+      {
+        title: "تبلیغات ویدیویی گوگل",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "نمایش تبلیغ شما در ویدیوها",
+      },
+      {
+        title: "اکانت اختصاصی گوگل ادز",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "دسترسی مستقیم به اکانت خود",
+      },
+      {
+        title: "نمایندگی گوگل ادز",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "نماینده تبلیغات در گوگل",
+      },
+    ],
+  },
+  {
+    title: "سرور",
+    children: [
+      {
+        title: "سرور مجازی",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "میزبانی شده در دیتاسنتر اختصاصی برتینا",
+      },
+      {
+        title: "سرور مجازی ایران",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "میزبانی شده در دیتاسنتر اختصاصی برتینا",
+      },
+      {
+        title: "سرور مجازی آلمان",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "میزبانی شده در دیتاسنتر Hetzner آلمان",
+      },
+      {
+        title: "سرور مجازی فرانسه",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "میزبانی شده در دیتاسنتر OVH فرانسه",
+      },
+      {
+        title: "سرور اختصاصی ایران",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "سخت‌افزار اختصاصی و پینگ مناسب",
+      },
+      {
+        title: "سرور اختصاصی آلمان",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "سرعت بالا و سخت‌افزار قوی",
+      },
+      {
+        title: "میزبانی سرور (کولوکیشن)",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "دیتاسنتر امن برتینا با اینترنت پایدار",
+      },
+    ],
+  },
+  {
+    title: "ایمیل سازمانی",
+    children: [],
+  },
+  {
+    title: "خرید SSL",
+    children: [],
+  },
+  {
+    title: "درباره ما",
+    children: [
+      {
+        title: "وبلاگ",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "جهت مشاهده وبلاگ برتینا کلیک کنید",
+      },
+      {
+        title: "درباره ما",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "درباره ما",
+      },
+      {
+        title: "تماس با ما",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "راه‌های ارتباطی با برتینا",
+      },
+      {
+        title: "ناحیه کاربری برتینا",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "ورود به ناحیه کاربری",
+      },
+      {
+        title: "شماره حساب‌ها",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "روش‌های پرداخت و شماره حساب‌ها",
+      },
+      {
+        title: "استخدام",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "فرصت‌های شغلی ارائه شده در برتینا",
+      },
+      {
+        title: "سوالات متداول FAQ",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "پرسش و پاسخ‌های برتینا",
+      },
+      {
+        title: "شرایط و قوانین",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "شرایط و قوانین استفاده از خدمات",
+      },
+      {
+        title: "مجوزها و تاییدیه‌ها",
+        icon: "images/category/host/server-svgrepo-com.svg",
+        desc: "گواهی‌ها، تقدیرنامه‌ها و افتخارات",
+      },
+    ],
+  },
+];
